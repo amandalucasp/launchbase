cfb1260e965afee1b3d6a6fbaf1b564aee192143
Amanda Lucas <mandspereira@gmail.com>
2020-07-25 18:33:30 -0300

    modulo 03 finished classes

---
+++ modulo03/data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "2alg7MQ6_sI",
+        title: "Github na prática | Rocketseat",
+        duration: "34 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid | Rocketseat",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "c7P03kkrEG8",
+        title: "Configurando VS Code | Rocketseat",
+        duration: "34 min",
+        price: "Free"
+    },
+    {
+        id: "u99tNt3TZf8",
+        title: "Desvendando Redux | Rocketseat",
+        duration: "51 min",
+        price: "Free"
+    },
+    {
+        id: "H91DhKPjhPk",
+        title: "Responsividade na prática | Rocketseat",
+        duration: "34 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "DDm0M_rZLJo",
+        title: "Clonando Interface do Nubank | Rocketseat",
+        duration: "70 min",
+        price: "Free",
+        featured: true
+    }
+]
--- modulo03/server.js
+++ modulo03/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public')) /*observa a pasta public*/
 
@@ -9,19 +10,45 @@ server.use(express.static('public')) /*observa a pasta public*/
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res) {
     // return res.send("hola")
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/67033667?s=460&u=e1cd212cd3fc9cdbae7e986c097a3a6dfe164136&v=4",
+        name: "Amanda Lucas",
+        role: "Instrutora - Rocketseat",
+        description: 'Programadora full-stack, focada em trazer o melhor ensino na <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>. Formada pelo Ifes da Serra/ES.',
+        links: [
+            {name: "Github", url:"https://github.com/amandalucs"},
+            {name: "Linkedin", url:"https://www.linkedin.com/in/amandalpereira/"},
+            {name: "Instagram", url:"https://www.instagram.com/amandalucasp/"}
+        ]
+    }
+
+    return res.render("about", { about: about })
 })
 
 server.get("/portfolio", function(req, res) {
-    // return res.send("hola")
-    return res.render("portfolio")
+    return res.render("portfolio", {items: videos})
 })
 
+server.get("/video", function(req, res){
+    const id = req.query.id
+
+    const video = videos.find(function(video){
+        return video.id == id /* se achar, é true */
+    })
+
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", { item: video })
+})
 
 server.listen(5000, function() {
     console.log("server is running")
